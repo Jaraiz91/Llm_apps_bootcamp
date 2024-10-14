e7a4692d1c857b3ffbb7ed2280c206636e519db2
Javier Araiz <jaraiz37@gmail.com>
2024-10-14 15:30:27 +0200

    key extractor

--- level_2_llm_apps/key_data_extractor/main.py
+++ level_2_llm_apps/key_data_extractor/main.py
@@ -13,18 +13,33 @@ class PageClassifier(BaseModel):
     language : str = Field(description='The language the text is written with')  
 
 def load_page_content(url):
-    loader = WebBaseLoader()
+    loader = WebBaseLoader(url)
     docs = loader.load()
     content = ' '.join([x.page_content for x in docs])
     return content
 
+classify_prompt_template = """Your task is to extract the information from the following passage.
+Use the format provided for the output. Provide the answer in english
+passge:
+{input}"""
+
+classify_prompt = ChatPromptTemplate.from_template(classify_prompt_template)
+summary_prompt_template = """Your task is to provide a summary in English no longer than 50 words to explain
+{input}"""
+summary_prompt = ChatPromptTemplate.from_template(summary_prompt_template)
 st.markdown('# **Key data extractor**')
 
-def get_llm(model,api_key):
+def get_llm(model,api_key, schema=False):
     if model == 'gpt-3.5':
-        llm = ChatOpenAI(model='gpt-3.5-turbo', api_key=api_key)
+        if schema == True:
+            llm = ChatOpenAI(model='gpt-3.5-turbo', api_key=api_key).with_structured_output(schema=PageClassifier)
+        else:
+            llm = ChatOpenAI(model='gpt-3.5-turbo', api_key=api_key)
     else:
-        llm = ChatGroq(model='llama-3.1-70b-versatile', api_key=api_key)
+        if schema == True:
+            llm = ChatGroq(model='llama-3.1-70b-versatile', api_key=api_key).with_structured_output(schema=PageClassifier)
+        else:
+            llm = ChatGroq(model='llama-3.1-70b-versatile', api_key=api_key)
     return llm
 
 col1, col2 = st.columns(2)
@@ -37,3 +52,66 @@ with col2:
 st.markdown('Provide a url to be analyzed:')
 
 url = st.text_input(label='url:')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    reset = st.button('reset', use_container_width=True)
+with col2:
+    button = st.button('Analyze', use_container_width=True)
+
+if reset:
+    st.stop()
+
+if button:
+    warning_api = "Please insert a valid api Key!"
+    if url:
+        # Check for the Api Key
+        if api_key:
+            try:
+                llm_with_schema = get_llm(model=model,api_key=api_key, schema=True)
+                llm = get_llm(model=model,api_key=api_key)
+
+            except:
+                st.warning(warning_api, icon="⚠️")
+                st.stop()
+        else:
+            st.warning(warning_api, icon="⚠️")
+            st.stop()
+        # checking the url
+        try:
+            content = load_page_content(url=url)
+        except:
+            not_valid_url = 'Seems the url provided is not valid, please try again with another one'
+            st.warning(not_valid_url, icon="⚠️")
+
+
+        classify_chain = classify_prompt | llm_with_schema
+        classified_url = classify_chain.invoke({'input': content})
+        summary_chain = summary_prompt | llm | StrOutputParser()
+        summary_response = summary_chain.invoke({'input': content})
+
+        topic, sentiment, language = st.columns(3)
+        with topic:
+            st.markdown('### **Topic:**')
+            st.write(classified_url.topic)
+        with sentiment:
+            st.markdown('### **Sentiment:**')
+            st.write(classified_url.sentiment)
+        with language:
+            st.markdown('### **Language:**')
+            st.write(classified_url.language)
+        
+        st.markdown('## **Summary:**')
+        st.write(summary_response)
+
+
+    else:
+        not_url = 'Please provide a valid url'
+        st.warning(not_url, icon="⚠️")
+
+
+
+        
+
+        
